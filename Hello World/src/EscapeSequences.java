fcd5e9aa93122af719719596ebcfd9dba4f3262c
E114JC <93293189+E114JC@users.noreply.github.com>
2025-01-10 13:21:46 -0500

    Add files via upload

---
+++ Hello World/src/EscapeSequences.java
@@ -0,0 +1,21 @@
+
+public class EscapeSequences {
+
+	public static void main(String[] args) {
+			//examples of escape sequences 
+//			System.out.println("Hello World");
+//			System.out.println("\tThis is a tab");
+//			System.out.println("First\nSecond");
+//			System.out.println("First\n\tSecond");
+//			System.out.println("\\");
+//			System.out.println("\"quote\"");
+//			System.out.println("\'single quote\'");
+//			System.out.println("Col\u00f3n");
+			//System.out.println("\u03c0");
+			
+			//concatenation
+			System.out.println("Name\t\tNickname\tHometown\n/=======\\\t/=======\\\t/=======\\\nStephanie\t\"Steph\"\t\tBarnaget\nMichael\t\t\"Mike\"\t\tBrick\nChristopher\t\"Chris\"\t\tToms River");
+			
+	}
+
+}
---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		//System.out.println("Hi person");
+		//System.out.println("How're you?");
+		//System.out.println("I'm good, thanks");
+		//System.out.println("tycruiovjfctyu");
+}
+
+}
